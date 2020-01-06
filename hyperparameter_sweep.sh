495984844a9f55e598f8bcf1cb175ca5d33c6cfc
Matthew Walker <kettch13@gmail.com>
2020-01-06 12:25:03 -0800

    add early stopping

--- hyperparameter_sweep.sh
+++ hyperparameter_sweep.sh
@@ -20,7 +20,7 @@ do
       PREFIX="${MODEL}_${LAYER_NUM}_${DATASET}_L1${LAMBDA1}_L2${LAMBDA2}_A${ALPHA}_LR${LR}_WD${WEIGHT_DECAY}"
       TMP_DIR_NAME="tmp_${PREFIX}"
       mkdir $TMP_DIR_NAME
-      python main.py --model $MODEL --layer_num $LAYER_NUM --dataset $DATASET --cpu --lambda1 $LAMBDA1 --lambda2 $LAMBDA2 --alpha $ALPHA --lr $LR --epoch_num $EPOCH_NUM --epoch_log $EPOCH_LOG --repeat_num $REPEAT_NUM --weight_decay $WEIGHT_DECAY >& $TMP_DIR_NAME/$FULL_LOG_NAME
+      python main.py --model $MODEL --layer_num $LAYER_NUM --dataset $DATASET --cpu --lambda1 $LAMBDA1 --lambda2 $LAMBDA2 --alpha $ALPHA --lr $LR --epoch_num $EPOCH_NUM --epoch_log $EPOCH_LOG --repeat_num $REPEAT_NUM --weight_decay $WEIGHT_DECAY >& $TMP_DIR_NAME/$FULL_LOG_NAME --early_stopping True
       python analyzeLogs.py --filename $TMP_DIR_NAME/$FULL_LOG_NAME --prefix $PREFIX
       # rm -rf $tmpdirname
     done
