eac1b0f32f134d0b1f2a1f84ced57bfdf9ecba4e
Yafei Wang <yawang1@linkedin.com>
2020-01-17 15:12:07 -0800

    Add a new parameter sweep

--- hyperparameter_sweep_gpu_newSweep.sh
+++ hyperparameter_sweep_gpu_newSweep.sh
@@ -4,17 +4,15 @@
 DATASET=$1
 MODEL=$2
 
-
 LAYER_NUM=2
 #LAMBDA1=1
 #LAMBDA2=0
 WEIGHT_DECAY=0.01
 ALPHA=1
 EPOCH_LOG=5
-EPOCH_NUM=50
-REPEAT_NUM=5
-TMP_DIR_NAME="sweep_results/tmp_${MODEL}_${DATASET}_MSE"
-
+EPOCH_NUM=2000
+REPEAT_NUM=2
+TMP_DIR_NAME="sweep_results/tmp_${MODEL}E_${DATASET}_combined"
 
 rm -rf $TMP_DIR_NAME
 mkdir -p $TMP_DIR_NAME
@@ -25,13 +23,13 @@ for LAMBDA1 in 0.0 0.1 1 10;
     do			      
     for LR in 0.01 0.001 0.0001;
       do
-      for WEIGHT_DECAY in 0.01 0.001 0.0001;
+      for WEIGHT_DECAY in 0.1 0.01 0.001 0.0001;
         do
 	PREFIX="${LAYER_NUM}_${DATASET}_L1_${LAMBDA1}_L2_${LAMBDA2}_A${ALPHA}_LR${LR}_WD${WEIGHT_DECAY}"
 	LOG_FILE_NAME=`echo $PREFIX | sed 's/\./d/g'`
 	LOG_FILE_PATH="${TMP_DIR_NAME}/${LOG_FILE_NAME}"
 	SUMMARY_PATH="${TMP_DIR_NAME}/summary"
-        python main.py --model $MODEL --layer_num $LAYER_NUM --dataset $DATASET --gpu GPU --cuda $CUDA --lambda1 $LAMBDA1 --lambda2 $LAMBDA2 --alpha $ALPHA --lr $LR --epoch_num $EPOCH_NUM --epoch_log $EPOCH_LOG --repeat_num $REPEAT_NUM --weight_decay $WEIGHT_DECAY --early_stopping True |tee -a $LOG_FILE_PATH
+        python main.py --model $MODEL --cpu --hash_concat --layer_num $LAYER_NUM --dataset $DATASET --lambda1 $LAMBDA1 --lambda2 $LAMBDA2 --alpha $ALPHA --lr $LR --epoch_num $EPOCH_NUM --epoch_log $EPOCH_LOG --repeat_num $REPEAT_NUM --weight_decay $WEIGHT_DECAY --early_stopping True |tee -a $LOG_FILE_PATH
         python analyzeLogs.py --filename $LOG_FILE_PATH --prefix $PREFIX | tee -a $SUMMARY_PATH
         done
     done
